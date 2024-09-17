421b226b58a68c576b3781117b0949c65b9031e3
masky5859 <mathsuky2406@gmail.com>
2024-09-17 15:12:39 +0900

    feat: tailwindcssのlintを導入

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,18 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
+    'plugin:tailwindcss/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'tailwindcss/classnames-order': 'error',
+    'tailwindcss/no-arbitrary-value': 'warn'
   }
 }
