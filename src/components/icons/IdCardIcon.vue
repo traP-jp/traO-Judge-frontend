a79a3165e598b52eb78db3cbb93b108bc8eaaca2
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-09-27 21:42:15 +0900

    Material Symbolsフォントの読み込み・使うアイコンのみコンポーネント化

---
+++ src/components/icons/IdCardIcon.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const {size = "1.5rem", isFilled = false} = defineProps<{
+  size?: string,
+  isFilled?: boolean,
+}>()
+</script>
+
+<template>
+  <span
+    class="material-symbols-rounded"
+    :style="{fontVariationSettings: '\'FILL\' ' + (isFilled ? 1 : 0), fontSize: size}"
+  >
+    id_card
+  </span>
+</template>
---
+++ src/components/icons/LogoutIcon.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const {size = "1.5rem"} = defineProps<{
+  size?: string,
+}>()
+</script>
+
+<template>
+  <span
+    class="material-symbols-rounded"
+    :style="{fontVariationSettings: '\'FILL\' 0', fontSize: size}"
+  >
+    logout
+  </span>
+</template>
