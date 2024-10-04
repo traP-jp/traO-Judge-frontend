8f7749c41f1d6c3e26524e1e7b326277af4d8ac1
YuHima03 <yuhima03@gmail.com>
2024-10-04 23:05:52 +0900

    BottomNavbar (フッター) を実装

---
+++ src/components/Navigations/BottomNavbar.vue
@@ -0,0 +1,9 @@
+<script setup lang="tsx"></script>
+
+<template>
+  <footer class="fontstyle-ui-control m-0 flex w-full flex-col gap-4 bg-background-inv-primary px-8 py-4 text-text-inv-primary">
+    <div class="m-0 w-full p-0">
+      © 2024 Digital Creators Club traP
+    </div>
+  </footer>
+</template>
