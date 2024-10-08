83d345e31d4298b2714050fbc342c11418c19273
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-10-08 13:39:35 +0900

    BorderedButtonの追加

---
+++ src/components/Controls/BorderedButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { disabled = false } = defineProps<{
+  disabled?: boolean
+  text: string
+}>()
+const emit = defineEmits(['click'])
+function onClick() {
+  emit('click')
+}
+</script>
+
+<template>
+  <button
+    :disabled="disabled"
+    class="fontstyle-ui-control-strong rounded border border-brand-primary px-6 py-1.75 text-brand-primary enabled:hover:bg-brand-light-primary disabled:opacity-50"
+    @click="onClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style scoped></style>
