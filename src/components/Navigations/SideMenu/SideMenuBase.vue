15b624daba6b05bd689b83017f6d747c9557d7a2
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-10-08 18:40:40 +0900

    ベース+利用例3種のSideMenuを追加

---
+++ src/components/Navigations/SideMenu/SideMenuBase.vue
@@ -0,0 +1,57 @@
+<script setup lang="ts">
+import MenuButton from '@/components/Navigations/MenuButton.vue'
+import type { Icon } from '@/components/MaterialIcon.vue'
+import { ref } from 'vue'
+
+export type SideMenuProps = {
+  icon: Icon
+  text: string
+  onClick: () => void
+}
+const { bottomContents = [] } = defineProps<{
+  mainContents: SideMenuProps[]
+  bottomContents?: SideMenuProps[]
+}>()
+
+const currentTab = ref(0)
+</script>
+
+<template>
+  <nav class="flex h-full w-74.5 flex-col px-6 py-12">
+    <slot></slot>
+    <div class="flex grow flex-col">
+      <ul class="mt-6 flex flex-col gap-1">
+        <li v-for="content in mainContents" :key="content.text">
+          <MenuButton
+            :icon="content.icon"
+            :text="content.text"
+            :selected="mainContents.indexOf(content) === currentTab"
+            @click="
+              () => {
+                currentTab = mainContents.indexOf(content)
+                content.onClick()
+              }
+            "
+          />
+        </li>
+      </ul>
+      <ul class="mt-auto flex flex-col gap-1">
+        <li v-for="content in bottomContents" :key="content.text">
+          <MenuButton
+            :icon="content.icon"
+            :text="content.text"
+            :selected="mainContents.length + bottomContents.indexOf(content) === currentTab"
+            @click="
+              () => {
+                currentTab = mainContents.length + bottomContents.indexOf(content)
+                content.onClick()
+              }
+            "
+          />
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped></style>
---
+++ src/components/Navigations/SideMenu/SideMenuUserSetting.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import SideMenuBase, {
+  type SideMenuProps
+} from '@/components/Navigations/SideMenu/SideMenuBase.vue'
+
+const mainContents: SideMenuProps[] = [
+  {
+    text: 'アカウント',
+    icon: 'person',
+    onClick: () => {
+      console.log('TODO: アカウント')
+    }
+  },
+  {
+    text: 'プロフィール',
+    icon: 'id_card',
+    onClick: () => {
+      console.log('TODO: プロフィール')
+    }
+  }
+]
+</script>
+
+<template>
+  <SideMenuBase :main-contents="mainContents" :bottom-contents="bottomContents">
+    <h1 class="fontstyle-ui-subtitle">設定</h1>
+  </SideMenuBase>
+</template>
+
+<style scoped></style>
