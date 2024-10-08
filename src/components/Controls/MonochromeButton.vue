7f376e027c683c766ecc8118a4127a2c6d522bfe
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-10-08 13:51:18 +0900

    MonochromeButtonの追加

---
+++ src/components/Controls/MonochromeButton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { disabled = false } = defineProps<{
+  disabled?: boolean
+  text: string
+}>()
+const emit = defineEmits(['click'])
+function onClick() {
+  emit('click')
+}
+</script>
+
+<template>
+  <button
+    :disabled="disabled"
+    class="fontstyle-ui-control-strong rounded border border-text-primary px-6 py-1.75 text-text-primary enabled:hover:bg-background-secondary disabled:opacity-50"
+    @click="onClick"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<style scoped></style>
