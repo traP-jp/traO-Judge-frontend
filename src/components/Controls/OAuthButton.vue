60252d0dd6fb07ba559ef7fa512e8c122e0ddb82
Eraxyso <130852025+Eraxyso@users.noreply.github.com>
2024-12-12 12:42:52 +0000

    feat: implement SignupView

---
+++ src/components/Controls/OAuthButton.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const { disabled = false } = defineProps<{
+  disabled?: boolean
+  app: string
+  mode: 'signup' | 'login'
+}>()
+function onOAuthClick() {
+  // TODO: some OAuth login/signup logic
+}
+</script>
+
+<template>
+  <button
+    :disabled="disabled"
+    class="fontstyle-ui-control-strong inline-block space-x-2.5 rounded-lg border border-border-secondary px-3 py-2 text-text-primary enabled:hover:bg-background-secondary disabled:opacity-50"
+    @click="onOAuthClick"
+  >
+    <span v-if="app === 'Github'" class="inline-block align-middle"
+      ><img src="" class="size-5"
+    /></span>
+    <span v-if="app === 'Google'" class="inline-block align-middle"
+      ><img src="" class="size-5"
+    /></span>
+    <span v-if="app === 'traQ'" class="inline-block align-middle"
+      ><img src="" class="size-5"
+    /></span>
+    <span class="inline-block align-middle">{{ app }} で新規登録</span>
+  </button>
+</template>
+
+<style scoped></style>
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,8 +1,42 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import EmailTextbox from '@/components/Controls/Textbox/EmailTextbox.vue'
+import PrimaryButton from '@/components/Controls/PrimaryButton.vue'
+import OAuthButton from '@/components/Controls/OAuthButton.vue'
+
+const emailAddress = ref('')
+function onEmailSignup() {
+  // TODO: Implement email signup
+}
+</script>
 
 <template>
-  <div>
-    <h1>Signup</h1>
+  <div
+    class="flex items-center justify-center bg-background-tertiary px-8 py-6"
+    style="height: calc(100vh - 56px)"
+  >
+    <div class="max-w-3xl space-y-5 rounded-2xl bg-white px-14 py-10">
+      <div class="fontstyle-ui-title text-left">新規登録</div>
+      <div class="flex divide-x divide-border-secondary">
+        <div class="flex-1 pr-4">
+          <div class="space-y-3 p-2.5">
+            <OAuthButton app="Github" mode="signup" class="w-full" />
+            <OAuthButton app="Google" mode="signup" class="w-full" />
+            <OAuthButton app="traQ" mode="signup" class="w-full" />
+          </div>
+        </div>
+        <div class="flex-1 pl-4">
+          <div class="space-y-3 p-2.5">
+            <div class="fontstyle-ui-body-strong text-left">メールアドレスで新規登録</div>
+            <EmailTextbox v-model="emailAddress" placeholder="メールアドレス" />
+            <PrimaryButton text="次へ" class="w-full" @click="onEmailSignup" />
+            <div class="fontstyle-ui-caption-link text-left">
+              <a href="login">すでにアカウントをお持ちの場合</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
