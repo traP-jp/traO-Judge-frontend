b2b9f3ccbe8ca10ba9ba2cadf7adcb3c3b53f8a0
kitsne241 <bicycle.y.t@gmail.com>
2024-12-20 09:07:51 +0900

    フォントスタイルの適用とmain.cssの修正

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,26 +11,26 @@ import PasswordTextbox from '@/components/Controls/Textbox/PasswordTextbox.vue'
     class="flex items-center justify-center bg-background-secondary"
   >
     <div class="w-200 rounded-2xl bg-background-primary px-14 py-10">
-      <h1 class="mb-5 h-9 font-primary text-28px font-medium text-[#1E1E1E]">ログイン</h1>
+      <h1 class="fontstyle-ui-title mb-5 text-[#1E1E1E]">ログイン</h1>
       <div class="flex h-44 items-start">
         <div class="flex w-82 flex-col p-2.5">
           <button
             class="mb-3 flex h-10 w-full items-center justify-center rounded-lg border border-border-secondary"
           >
             <img src="@/assets/service_icons/github.svg" class="mr-2.5 h-5" />
-            <span class="font-primary text-14px font-medium text-[#3A3A3A]">GitHub でログイン</span>
+            <span class="fontstyle-ui-control-strong text-[#3A3A3A]">GitHub でログイン</span>
           </button>
           <button
             class="mb-3 flex h-10 w-full items-center justify-center rounded-lg border border-border-secondary"
           >
             <img src="@/assets/service_icons/google.svg" class="mr-2.5 h-5" />
-            <span class="font-primary text-14px font-medium text-[#3A3A3A]">Google でログイン</span>
+            <span class="fontstyle-ui-control-strong text-[#3A3A3A]">Google でログイン</span>
           </button>
           <button
             class="mb-3 flex h-10 w-full items-center justify-center rounded-lg border border-border-secondary"
           >
             <img src="@/assets/service_icons/traq.svg" class="mr-2.5 h-5" />
-            <span class="font-primary text-14px font-medium text-[#3A3A3A]">traQ でログイン</span>
+            <span class="fontstyle-ui-control-strong text-[#3A3A3A]">traQ でログイン</span>
           </button>
         </div>
         <div class="mx-4 h-full border-r border-border-secondary"></div>
@@ -41,7 +41,7 @@ import PasswordTextbox from '@/components/Controls/Textbox/PasswordTextbox.vue'
             <PrimaryButton class="mr-3 h-full w-37 rounded-lg" text="ログイン" />
             <BorderedButton class="h-full w-37 rounded-lg" text="新規登録" />
           </div>
-          <router-link to="" class="h-4 text-12px font-normal text-[#3A3A3A] underline">
+          <router-link to="" class="fontstyle-ui-caption h-4 text-[#3A3A3A] underline">
             パスワードを忘れた場合</router-link
           >
         </div>
