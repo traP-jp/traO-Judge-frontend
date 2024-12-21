676554999f84a2abf0215ebb1a38716d61e39ce4
ZOI-dayo <zoizoi.dayo@gmail.com>
2024-12-21 12:46:52 +0900

    Button, Signupのフォーマット

--- src/utils/validator.ts
+++ src/utils/validator.ts
@@ -3,9 +3,9 @@ export const usernameValidator = (username: string): boolean => {
 }
 
 export const passwordValidator = (password: string): boolean => {
-  const hasLetter = /[A-Za-z]/.test(password);
-  const hasNumber = /[0-9]/.test(password);
-  const hasSpecialChar = /[!@#$%^&*()_+\-={}[\]:;"'<>,.?/]/.test(password);
-  const isValid = /^[A-Za-z0-9!@#$%^&*()_+\-={}[\]:;"'<>,.?/]{10,64}$/.test(password);
-  return hasLetter && hasNumber && hasSpecialChar && isValid;
+  const hasLetter = /[A-Za-z]/.test(password)
+  const hasNumber = /[0-9]/.test(password)
+  const hasSpecialChar = /[!@#$%^&*()_+\-={}[\]:;"'<>,.?/]/.test(password)
+  const isValid = /^[A-Za-z0-9!@#$%^&*()_+\-={}[\]:;"'<>,.?/]{10,64}$/.test(password)
+  return hasLetter && hasNumber && hasSpecialChar && isValid
 }
