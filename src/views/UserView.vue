9c8e614a5e394c3500946c3a3ec1d5eee6be84ca
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-12-24 02:23:28 +0900

    Add user sidebar in user pages

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,9 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import SideMenuUserPage from '@/components/Navigations/SideMenu/SideMenuUserPage.vue'
+
+const route = useRoute()
+
+if (typeof route.params.id !== 'string') throw new Error('Invalid route')
+const username = route.params.id
+// TODO: Fetch user data
+</script>
 
 <template>
-  <div>
-    <h1>User</h1>
-    <RouterView />
+  <div class="flex gap-[10vw] px-[10vw]">
+    <nav class="sticky top-14 h-[calc(100vh-3.5rem)]">
+      <SideMenuUserPage :username="username" />
+    </nav>
+    <main class="flex-auto py-10">
+      <RouterView />
+    </main>
   </div>
 </template>
 
--- src/views/user/UserSubmissions.vue
+++ src/views/user/UserSubmissions.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="rounded-lg border border-solid border-[#D8D8D8]">
     <h2>User Submissions</h2>
   </div>
 </template>
