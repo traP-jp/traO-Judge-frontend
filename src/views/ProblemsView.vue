3a4a18d4dc3f624aea2de031f24f990e20b963c4
masky5859 <mathsuky2406@gmail.com>
2024-12-26 23:35:59 +0900

    Revert "fix:CIで実行するコマンドを変更"
    This reverts commit 8bd2217f3bdcee9271aaae93a7fdca8dbb22c613.

--- src/views/ProblemsView.vue
+++ src/views/ProblemsView.vue
@@ -1,16 +1,9 @@
+<script setup lang="ts"></script>
+
 <template>
   <div>
-    <h1>問題詳細</h1>
-    <!-- 他の問題詳細情報を表示する部分 -->
-    <SubmitForm :problem-id="problemId" />
+    <h1>Problems</h1>
   </div>
 </template>
-
-<script setup lang="ts">
-import { ref } from 'vue'
-import SubmitForm from '@/components/SubmitForm.vue'
-
-const problemId = ref(1) // ここで問題IDを設定します
-</script>
 
 <style scoped></style>
