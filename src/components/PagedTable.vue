04286db88e2d84903c68eb30396d56193dd31dde
TwoSquirrels <RisuRisu.TwoSquirrels@gmail.com>
2024-12-26 09:49:48 +0900

    Add PagedTable component

---
+++ src/components/PagedTable.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const { colIds, rowIds } = defineProps<{
+  colIds: string[]
+  rowIds: string[]
+}>()
+</script>
+
+<template>
+  <table class="w-full table-fixed">
+    <thead class="h-10 bg-[#F5F5F5]">
+      <tr>
+        <th v-for="colId in colIds" :key="colId">
+          <slot name="head" :col-id="colId" />
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="rowId in rowIds" :key="rowId">
+        <th v-for="colId in colIds" :key="colId">
+          <slot name="cell" :row-id="rowId" :col-id="colId" />
+        </th>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped></style>
--- src/views/user/UserSubmissions.vue
+++ src/views/user/UserSubmissions.vue
@@ -1,69 +1,112 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PagedTable from '@/components/PagedTable.vue'
+import type { JudgeStatus } from '@/api/generated'
+
+const colIds = [
+  'submittedAt',
+  'userName',
+  'totalScore',
+  'codeLength',
+  'judgeStatus',
+  'maxTime',
+  'maxMemory'
+]
+
+const submissions: {
+  id: string
+  submittedAt: string
+  userName: string
+  totalScore: number
+  codeLength: number
+  judgeStatus: JudgeStatus
+  maxTime: number
+  maxMemory: number
+}[] = [
+  {
+    id: '000011',
+    submittedAt: '1234/56/78 90:56:78',
+    userName: 'test_user',
+    totalScore: 400,
+    codeLength: 2048,
+    judgeStatus: 'AC',
+    maxTime: 123,
+    maxMemory: 12345
+  },
+  {
+    id: '000007',
+    submittedAt: '1234/56/78 90:45:67',
+    userName: 'test_user',
+    totalScore: 0,
+    codeLength: 2048,
+    judgeStatus: 'WA',
+    maxTime: 123,
+    maxMemory: 12345
+  },
+  {
+    id: '000005',
+    submittedAt: '1234/56/78 90:34:56',
+    userName: 'test_user',
+    totalScore: 300,
+    codeLength: 1024,
+    judgeStatus: 'AC',
+    maxTime: 12,
+    maxMemory: 12345
+  },
+  {
+    id: '000003',
+    submittedAt: '1234/56/78 90:23:45',
+    userName: 'test_user',
+    totalScore: 200,
+    codeLength: 512,
+    judgeStatus: 'AC',
+    maxTime: 12,
+    maxMemory: 12345
+  },
+  {
+    id: '000002',
+    submittedAt: '1234/56/78 90:12:34',
+    userName: 'test_user',
+    totalScore: 100,
+    codeLength: 256,
+    judgeStatus: 'AC',
+    maxTime: 1,
+    maxMemory: 1234
+  }
+]
+</script>
 
 <template>
   <div class="rounded-lg border border-solid border-[#D8D8D8] pt-28 text-center">
     <h2 class="fontstyle-ui-title-large">提出一覧<br />テーブル</h2>
     <section class="p-10">
-      <table class="w-full table-fixed">
-        <thead class="h-10 bg-[#F5F5F5]">
-          <tr>
-            <th>提出日時</th>
-            <th>ユーザー名</th>
-            <th>得点</th>
-            <th>コード長</th>
-            <th>ジャッジ結果</th>
-            <th>実行時間</th>
-            <th>メモリ</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1234/56/78 90:56:78</td>
-            <td>test_user</td>
-            <td>400</td>
-            <td>2048 Byte</td>
-            <td>AC</td>
-            <td>123 ms</td>
-            <td>12345 KB</td>
-          </tr>
-          <tr>
-            <td>1234/56/78 90:45:67</td>
-            <td>test_user</td>
-            <td>0</td>
-            <td>2048 Byte</td>
-            <td>WA</td>
-            <td>123 ms</td>
-            <td>12345 KB</td>
-          </tr>
-          <tr>
-            <td>1234/56/78 90:34:56</td>
-            <td>test_user</td>
-            <td>300</td>
-            <td>1024 Byte</td>
-            <td>AC</td>
-            <td>12 ms</td>
-            <td>12345 KB</td>
-          </tr>
-          <tr>
-            <td>1234/56/78 90:23:45</td>
-            <td>test_user</td>
-            <td>200</td>
-            <td>512 Byte</td>
-            <td>AC</td>
-            <td>12 ms</td>
-            <td>12345 KB</td>
-          </tr>
-          <tr>
-            <td>1234/56/78 90:12:34</td>
-            <td>test_user</td>
-            <td>100</td>
-            <td>256 Byte</td>
-            <td>AC</td>
-            <td>1 ms</td>
-            <td>1234 KB</td>
-          </tr>
-        </tbody>
-      </table>
+      <PagedTable :col-ids="colIds" :row-ids="submissions.map(({ id }) => id)">
+        <template #head="{ colId }">
+          {{
+            {
+              submittedAt: '提出日時',
+              userName: 'ユーザー名',
+              totalScore: '得点',
+              codeLength: 'コード長',
+              judgeStatus: 'ジャッジ結果',
+              maxTime: '実行時間',
+              maxMemory: 'メモリ'
+            }[colId]
+          }}
+        </template>
+        <template #cell="{ rowId, colId }">
+          {{
+            {
+              submittedAt: submissions.find(({ id }) => id === rowId)?.submittedAt,
+              userName: submissions.find(({ id }) => id === rowId)?.userName,
+              totalScore: submissions.find(({ id }) => id === rowId)?.totalScore,
+              codeLength: `${submissions.find(({ id }) => id === rowId)?.codeLength} Byte`,
+              judgeStatus: submissions.find(({ id }) => id === rowId)?.judgeStatus,
+              maxTime: `${submissions.find(({ id }) => id === rowId)?.maxTime} ms`,
+              maxMemory: `${submissions.find(({ id }) => id === rowId)?.maxMemory} KB`
+            }[colId]
+          }}
+        </template>
+      </PagedTable>
     </section>
   </div>
 </template>
