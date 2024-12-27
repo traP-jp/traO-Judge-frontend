78f529f7fb0171973547e04f834875838cc67468
masky5859 <mathsuky2406@gmail.com>
2024-12-27 23:31:59 +0900

    fix:sedコマンドの修正とエラーメッセージの修正

--- scripts/gen-api.sh
+++ scripts/gen-api.sh
@@ -1,12 +1,21 @@
+
 #!/bin/bash -eux
 
 # 引数で書き込むBASE_PATHを切り替える
+if [ "$#" -ne 1 ]; then
+  echo "Error: Incorrect number of arguments."
+  echo "Usage: $0 <environment>"
+  echo "Please specify the environment as 'production' or 'development', and provide a second argument."
+  exit 1
+fi
+
 if [ "$1" = "production" ]; then
   BASE_PATH="https://trao-judge-backend.trap.show/v0"
 elif [ "$1" = "development" ]; then
   BASE_PATH="http://localhost:4010"
 else
-  echo "Invalid argument: $1"
+  echo "Error: Invalid argument: $1"
+  echo "Please specify the environment as 'production' or 'development'."
   exit 1
 fi
 
@@ -19,11 +28,8 @@ docker run --rm -v "$(dirname $0)/../:/local" -u $(id -u):$(id -g) openapitools/
 # 生成されたファイルの中でBASE_PATHを置換
 GENERATED_FILE_PATH="$(dirname $0)/../src/api/generated/runtime.ts"
 
-# OSに応じてsedコマンドのオプションを切り替え
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # macOSの場合
-  sed -i '' "s|export const BASE_PATH = .*|export const BASE_PATH = '$BASE_PATH';|" $GENERATED_FILE_PATH
-else
-  # Linuxの場合
-  sed -i "s|export const BASE_PATH = .*|export const BASE_PATH = '$BASE_PATH';|" $GENERATED_FILE_PATH
-fi
+# sedコマンドでBASE_PATHを置換し，バックアップファイルを作成
+sed -i.bak "s|export const BASE_PATH = .*|export const BASE_PATH = '$BASE_PATH';|" $GENERATED_FILE_PATH
+
+# バックアップファイルを削除
+rm "${GENERATED_FILE_PATH}.bak"
