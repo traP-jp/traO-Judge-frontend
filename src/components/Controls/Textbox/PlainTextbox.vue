b47001f67d3d36010a0ff1e24d75fffb6716e67f
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-01-10 15:26:03 +0900

    EmailTextbox, PasswordTextboxをPlainTextboxを利用したものに変更

--- src/components/Controls/Textbox/PlainTextbox.vue
+++ src/components/Controls/Textbox/PlainTextbox.vue
@@ -5,20 +5,29 @@ import { computed, onMounted, ref, useTemplateRef } from 'vue'
 defineOptions({
   inheritAttrs: false
 })
-const { errorMessage = '', label = '' } = defineProps<{
-  errorMessage?: string
-  label?: string
-  isRequired?: boolean
-  id: string
-  leftIcon?: Icon
-  rightIcon?: Icon
-  isDisabled?: boolean
+const {
+  errorMessage = '',
+  label = '',
+  leftIcon,
+  rightIcon,
+  supportingText
+} = defineProps<{
+  disabled?: boolean
   displaysLength?: boolean
+  errorMessage?: string
+  id: string
+  label?: string
+  leftIcon?: Icon
+  required?: boolean
+  rightIcon?: Icon
   supportingText?: string
 }>()
 const emit = defineEmits(['clickLeft', 'clickRight'])
 const value = defineModel<string>()
-const isError = computed(() => errorMessage != '')
+const displaysError = computed(() => errorMessage != '')
+const displaysLeftIcon = computed(() => leftIcon != null)
+const displaysRightIcon = computed(() => rightIcon != null)
+const displaysSupportingText = computed(() => supportingText != null)
 const input = useTemplateRef('input')
 const isFocused = ref<boolean>(false)
 onMounted(() => {
@@ -31,21 +40,21 @@ onMounted(() => {
   <div class="flex flex-col gap-1">
     <span v-if="label != ''" class="flex items-center gap-2">
       <label class="fontstyle-ui-control text-text-primary" :for="id">{{ label }}</label>
-      <span v-if="isRequired" class="fontstyle-ui-caption-strong text-status-error">必須</span>
+      <span v-if="required" class="fontstyle-ui-caption-strong text-status-error">必須</span>
     </span>
     <span
       class="flex rounded border bg-background-primary px-2 py-1"
       :class="[
         { 'outline outline-1': isFocused },
-        { 'border-border-secondary outline-text-primary': !isError },
-        { 'border-status-error outline outline-1 outline-status-error': isError },
-        { 'border-text-primary': isFocused && !isError },
-        { 'bg-background-secondary': isDisabled }
+        { 'border-border-secondary outline-text-primary': !displaysError },
+        { 'border-status-error outline outline-1 outline-status-error': displaysError },
+        { 'border-text-primary': isFocused && !displaysError },
+        { 'bg-background-secondary': disabled }
       ]"
     >
       <MaterialIcon
-        v-if="leftIcon != undefined"
-        :icon="leftIcon"
+        v-if="displaysLeftIcon"
+        :icon="leftIcon!"
         size="1.25rem"
         @click="emit('clickLeft')"
       />
@@ -53,27 +62,27 @@ onMounted(() => {
         v-bind="$attrs"
         :id="id"
         ref="input"
-        v-model="value"
-        :disabled="isDisabled"
-        class="fontstyle-ui-body h-5 min-w-0 bg-transparent px-2 text-text-primary outline-none placeholder:text-text-tertiary"
+        v-model.lazy="value"
+        :disabled="disabled"
+        class="fontstyle-ui-body h-5 w-full min-w-0 bg-transparent px-2 text-text-primary outline-none placeholder:text-text-tertiary"
       />
       <span class="inline-flex items-center gap-2">
         <span v-if="displaysLength" class="fontstyle-ui-caption text-text-secondary">{{
           value?.length ?? 0
         }}</span>
         <MaterialIcon
-          v-if="rightIcon != undefined"
-          :icon="rightIcon"
+          v-if="displaysRightIcon"
+          :icon="rightIcon!"
           size="1.25rem"
           @click="emit('clickRight')"
         />
       </span>
     </span>
-    <span v-if="supportingText != undefined" class="fontstyle-ui-caption text-text-secondary">{{
+    <span v-if="displaysSupportingText" class="fontstyle-ui-caption text-text-secondary">{{
       supportingText
     }}</span>
   </div>
-  <div v-if="isError" class="mt-2 flex items-start gap-2 text-status-error">
+  <div v-if="displaysError" class="mt-2 flex items-start gap-2 text-status-error">
     <MaterialIcon icon="error" size="1.25rem" is-filled />
     <span class="fontstyle-ui-control min-w-0 break-words">{{ errorMessage }}</span>
   </div>
