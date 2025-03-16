eb2a5e341ef4aa50f9d71212d4c560397e03226f
masky5859 <mathsuky2406@gmail.com>
2025-03-16 15:13:16 +0900

    fix:typo

--- src/views/ResetPasswordSuccessView.vue
+++ src/views/ResetPasswordSuccessView.vue
@@ -5,7 +5,7 @@ import PrimaryButton from '@/components/Controls/PrimaryButton.vue'
 <template>
   <div class="flex h-header-offset items-start justify-center bg-background-secondary p-6">
     <div class="flex max-w-form-max flex-col gap-6 rounded-15 bg-background-primary px-14 py-10">
-      <h1 class="fontstyle-ui-title text-text-primary">パスワードの再設定しました</h1>
+      <h1 class="fontstyle-ui-title text-text-primary">パスワードを再設定しました</h1>
       <PrimaryButton class="h-10 w-full" @click="$router.push('/')"> トップに戻る </PrimaryButton>
     </div>
   </div>
