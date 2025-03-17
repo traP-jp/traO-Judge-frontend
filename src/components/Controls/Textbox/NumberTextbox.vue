7d5b5ad1de3daf6ad19910152a86b1c25de6f7fd
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-03-18 00:51:14 +0900

    fix: fix type error

--- src/components/Controls/Textbox/NumberTextbox.vue
+++ src/components/Controls/Textbox/NumberTextbox.vue
@@ -5,7 +5,7 @@ import { computed } from 'vue'
 const value = defineModel<number>()
 const valueString = computed<string>({
   get: () => {
-    return value.value?.toFixed()
+    return value.value!.toFixed()
   },
   set: (newValue: string) => {
     value.value = Number(newValue)
