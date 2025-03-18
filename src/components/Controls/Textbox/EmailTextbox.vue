f075172dbcffe21c96ac72a3ef0365a9307fa219
masky5859 <mathsuky2406@gmail.com>
2025-03-18 16:33:29 +0900

    fix:記法を簡単に

--- src/components/Controls/Textbox/EmailTextbox.vue
+++ src/components/Controls/Textbox/EmailTextbox.vue
@@ -10,18 +10,13 @@ const value = defineModel<string>()
 const isEmailError = computed(() => (value.value?.length ?? 0) > 0 && !isEmail(value.value ?? ''))
 const shownErrorMessage = ref<string | undefined>(props.errorMessage) // 初期値を設定
 
-watch(
-  () => props.errorMessage,
-  () => {
-    updateError()
-  }
-)
-
 const updateError = () => {
   shownErrorMessage.value = isEmailError.value
     ? 'メールアドレスの形式が正しくありません。'
     : props.errorMessage
 }
+
+watch(() => props.errorMessage, updateError)
 </script>
 
 <template>
