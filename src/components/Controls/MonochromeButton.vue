cc36230473e6fbf7671fc12635b67d6b5594ced9
ZOI-dayo <zoizoi.dayo@gmail.com>
2025-03-20 23:01:37 +0900

    fix:MonochromeButtonのBorderの色を修正

--- src/components/Controls/MonochromeButton.vue
+++ src/components/Controls/MonochromeButton.vue
@@ -14,8 +14,8 @@ function onClick() {
 <template>
   <button
     :disabled="disabled"
-    class="fontstyle-ui-control-strong inline-block rounded border border-text-primary text-text-primary enabled:hover:bg-background-secondary disabled:opacity-50"
-    :class="small ? 'h-8 px-3' : 'h-10 px-6'"
+    class="fontstyle-ui-control-strong inline-block rounded border text-text-primary enabled:hover:bg-background-secondary disabled:opacity-50"
+    :class="small ? 'h-8 px-3 border-text-secondary' : 'h-10 px-6 border-text-primary'"
     :style="{ paddingLeft: padding, paddingRight: padding }"
     @click="onClick"
   >
