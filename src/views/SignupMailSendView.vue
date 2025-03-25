a443da44a1142dcff4a1130ad8eeaac1e1ddb7c2
masky5859 <mathsuky2406@gmail.com>
2025-03-25 19:19:34 +0900

    change:signuprequest周りのscriptを書き換え

--- src/views/SignupMailSendView.vue
+++ src/views/SignupMailSendView.vue
@@ -1,16 +1,55 @@
+`
 <script setup lang="ts">
+import { AuthenticationApi } from '@/api/generated'
+import { ResponseError } from '@/api/generated/runtime'
 import BorderedButton from '@/components/Controls/BorderedButton.vue'
 import PrimaryButton from '@/components/Controls/PrimaryButton.vue'
+import { useSignupStore } from '@/stores/signup'
+import { onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 
+const signupStore = useSignupStore()
 const router = useRouter()
 
-function onClose() {
+onMounted(() => {
+  // URL直打ち対策
+  if (!signupStore.isValid) {
+    router.push('/signup')
+  }
+})
+
+const handleClose = () => {
   router.push('/')
 }
 
-function onResendEmail() {
-  router.push('/signup')
+const handleResendEmail = async () => {
+  if (!signupStore.isValid) {
+    router.push('/signup')
+    return
+  }
+  const authApi = new AuthenticationApi()
+  try {
+    await authApi.postSignupRequest({
+      signupRequest: {
+        email: signupStore.email
+      }
+    })
+    // TODO: UIに含める？一旦仮置き
+    alert('メールを再送信しました。受信ボックスをご確認ください。')
+    signupStore.setEmail(signupStore.email)
+  } catch (error: unknown) {
+    console.error('メール再送信エラー:', error)
+    if (error instanceof ResponseError) {
+      const status = error.response.status
+      if (status === 400) {
+        alert('リクエストが不正です。入力内容を確認してください。')
+      } else {
+        alert('エラーが発生しました。もう一度お試しください。')
+      }
+    } else {
+      alert('予期せぬエラーが発生しました。もう一度お試しください。')
+    }
+  }
 }
 </script>
 
@@ -24,8 +63,10 @@ function onResendEmail() {
         60分以内に、メールに記載されたリンクから登録フォームにアクセスしてください。
       </p>
       <div class="flex flex-col items-stretch justify-center gap-2 self-stretch">
-        <PrimaryButton class="h-10" @click="onClose"> この画面を閉じる </PrimaryButton>
-        <BorderedButton class="h-10" @click="onResendEmail"> メールを再送信する </BorderedButton>
+        <PrimaryButton class="h-10" @click="handleClose"> この画面を閉じる </PrimaryButton>
+        <BorderedButton class="h-10" @click="handleResendEmail">
+          メールを再送信する
+        </BorderedButton>
       </div>
       <p class="fontstyle-ui-caption text-text-secondary">
         <span>メールアドレスを間違えた場合は、</span>
@@ -35,5 +76,4 @@ function onResendEmail() {
     </div>
   </div>
 </template>
-
-<style scoped></style>
+`
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
 import { AuthenticationApi } from '@/api/generated'
+import { ResponseError } from '@/api/generated/runtime'
 import OAuthButton from '@/components/Controls/OAuthButton.vue'
 import PrimaryButton from '@/components/Controls/PrimaryButton.vue'
 import EmailTextbox from '@/components/Controls/Textbox/EmailTextbox.vue'
+import { useSignupStore } from '@/stores/signup'
 import isEmail from 'validator/lib/isEmail'
 import { ref } from 'vue'
 import { useRouter } from 'vue-router'
@@ -10,8 +12,9 @@ import { useRouter } from 'vue-router'
 const email = ref('')
 const emailErrorMessage = ref<string | undefined>()
 const router = useRouter()
+const signupStore = useSignupStore()
 
-const onEmailSignup = async () => {
+const handleSignupRequest = async () => {
   if (!email.value) {
     emailErrorMessage.value = 'メールアドレスが入力されていません。'
     return
@@ -31,15 +34,21 @@ const onEmailSignup = async () => {
         email: email.value
       }
     })
-
+    signupStore.setEmail(email.value)
     router.push('/signup/mail-send')
-  } catch (error) {
-    console.error('サインアップエラー:', error)
-
-    if (error.response?.status === 400) {
-      emailErrorMessage.value = 'リクエストが不正です。入力内容を確認してください。'
+  } catch (error: unknown) {
+    if (error instanceof ResponseError) {
+      const status = error.response.status
+      // TODO: エラーハンドリングの文章
+      if (status === 400) {
+        emailErrorMessage.value = 'リクエストが不正です。入力内容を確認してください。'
+      } else {
+        emailErrorMessage.value = 'エラーが発生しました。もう一度お試しください。'
+      }
     } else {
-      emailErrorMessage.value = 'エラーが発生しました。もう一度お試しください。'
+      // TODO: エラーハンドリングの文章
+      emailErrorMessage.value = '予期せぬエラーが発生しました。もう一度お試しください。'
+      console.error(error)
     }
   }
 }
@@ -70,7 +79,7 @@ const onEmailSignup = async () => {
         <PrimaryButton
           class="h-10 w-full rounded px-6 py-2"
           :disabled="!email"
-          @click="onEmailSignup"
+          @click="handleSignupRequest"
         >
           メールアドレスで新規登録
         </PrimaryButton>
