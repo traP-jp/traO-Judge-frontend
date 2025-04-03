5a21b34fa74c20574ea4d598231318481467f5f7
ErrorSyntax1 <ken522.king@gmail.com>
2025-04-03 17:50:13 +0900

    fix: リンクの色とスタイルを修正

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -44,7 +44,7 @@ onMounted(() => {
   </div>
   <div class="fontstyle-ui-body self-stretch py-4">{{ selfIntroduction }}</div>
   <hr class="w-full border-border-primary" />
-  <div class="flex flex-col items-start gap-2 py-4">
+  <div class="fontstyle-ui-body flex flex-col items-start gap-2 py-4 text-text-primary">
     <div v-if="githubId && githubLink" class="flex items-center">
       <span>
         <img src="/src/assets/service_icons/github.svg" class="size-5" />
