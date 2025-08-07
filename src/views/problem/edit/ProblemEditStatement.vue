41b7305bdf6dc227c939777ce7c64f9b9e4e6513
otukado <otukado@gmail.com>
2025-08-08 01:04:03 +0900

    feat: 問題文の編集: サーバーとの通信を実装

--- src/views/problem/edit/ProblemEditStatement.vue
+++ src/views/problem/edit/ProblemEditStatement.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
 import { useRoute } from 'vue-router'
-import { ref, watch } from 'vue'
+import { ref, watch, onMounted } from 'vue'
 import PlainTextArea from '@/components/PlainTextArea.vue'
 import PrimaryButton from '@/components/Controls/PrimaryButton.vue'
+import { ProblemsApi, type Problem } from '@/api/generated'
+import AlertBox from '@/components/AlertBox.vue'
 
 const route = useRoute()
 
-const problemStatement = ref<string>('')
-
-const problemStatementError = ref<string>('')
+const problem = ref<Problem>()
+const body = ref<string>('')
 
 const saveError = ref<string>('')
 const saveSuccess = ref<boolean>(false)
@@ -25,9 +26,43 @@ watch(
   { immediate: true }
 )
 
+const problemsApi = new ProblemsApi()
+
+async function fetchProblem() {
+  isLoading.value = true
+  try {
+    problem.value = await problemsApi.getProblem({ problemId: problemId.value })
+    body.value = problem.value.statement
+  } catch (error) {
+    console.error('問題の取得に失敗しました:', error)
+    saveError.value = '問題の取得に失敗しました。'
+    saveErrorShow.value = true
+  } finally {
+    isLoading.value = false
+  }
+}
+
 async function saveStatement() {
   isLoading.value = true
+  try {
+    problem.value!.statement = body.value
+    await problemsApi.putProblem({
+      problemId: problemId.value,
+      putProblemRequest: problem.value
+    })
+    saveSuccess.value = true
+  } catch (error) {
+    console.error('問題文の保存に失敗しました:', error)
+    saveError.value = '問題文の保存に失敗しました。'
+    saveErrorShow.value = true
+  } finally {
+    isLoading.value = false
+  }
 }
+
+onMounted(() => {
+  fetchProblem()
+})
 </script>
 
 <template>
@@ -39,21 +74,23 @@ async function saveStatement() {
         <div class="w-full">
           <PlainTextArea
             id="problem-statement"
-            v-model="problemStatement"
+            v-model="body"
             class="h-64 w-full"
             required
-            :error-message="problemStatementError"
           />
         </div>
 
-        <PrimaryButton class="h-10 w-18 px-3 py-2" :disabled="isLoading">
+        <PrimaryButton
+          class="h-10 w-18 px-3 py-2"
+          :disabled="isLoading"
+          @click="saveStatement"
+        >
           保存
         </PrimaryButton>
 
         <AlertBox
           v-model:show="saveSuccess"
-          text="基本設定が保存されました。"
-          @click="saveStatement"
+          text="問題文が保存されました。"
         />
         <AlertBox v-model:show="saveErrorShow" :text="saveError" type="error" />
       </div>
