42378a3edd52697e5f7cc124a6ec4fc01e33f002
Eraxyso <130852025+Eraxyso@users.noreply.github.com>
2026-01-07 05:43:50 +0000

    fix: delete redirection to username input when register by traq as account with the same username as traq will be directly created

--- src/views/OAuthCallbackView.vue
+++ src/views/OAuthCallbackView.vue
@@ -72,7 +72,7 @@ onMounted(async () => {
 
       switch (action) {
         case 'signup':
-          await router.push('/signup/register?oauth=true&provider=traq')
+          await router.push('/')
           break
         case 'login':
           await userStore.fetchCurrentUser()
