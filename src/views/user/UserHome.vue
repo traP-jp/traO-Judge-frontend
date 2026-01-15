274cc86f4cc68ded159c421d90cd86477ab6f1da
masky5859 <mathsuky2406@gmail.com>
2026-01-15 17:37:13 +0900

    fix: account関連のリンクにユーザーのバリデーションを追加

--- src/views/user/UserHome.vue
+++ src/views/user/UserHome.vue
@@ -5,10 +5,42 @@ import type { User } from '@/api/generated'
 
 const { user } = defineProps<{ userId: string; user: User | null }>()
 
+/**
+ * GitHub username validation
+ * - Alphanumeric characters and hyphens only
+ * - Cannot start or end with a hyphen
+ * - No consecutive hyphens
+ * - 1-39 characters
+ * Source: https://github.com/shinnn/github-username-regex
+ *         https://docs.github.com/en/get-started/signing-up-for-github/signing-up-for-a-new-github-account
+ */
+const GITHUB_USERNAME_REGEX = /^[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,37}[a-zA-Z0-9])?$/
+
+/**
+ * X (Twitter) username validation
+ * - Alphanumeric characters and underscores only
+ * - 1-15 characters
+ * Source: https://help.x.com/en/managing-your-account/x-username-rules
+ */
+const X_USERNAME_REGEX = /^[A-Za-z0-9_]{1,15}$/
+
+const isValidGithubUsername = (username: string): boolean => {
+  if (!username || username.includes('--')) return false
+  return GITHUB_USERNAME_REGEX.test(username)
+}
+
+const isValidXUsername = (username: string): boolean => {
+  if (!username) return false
+  return X_USERNAME_REGEX.test(username)
+}
+
 const traqId = computed(() => user?.traqId ?? '')
 const githubId = computed(() => user?.githubId ?? '')
 const xId = computed(() => user?.xId ?? '')
 const selfIntroduction = computed(() => user?.selfIntroduction ?? '')
+
+const isGithubIdValid = computed(() => isValidGithubUsername(githubId.value))
+const isXIdValid = computed(() => isValidXUsername(xId.value))
 </script>
 
 <template>
@@ -23,7 +55,10 @@ const selfIntroduction = computed(() => user?.selfIntroduction ?? '')
         <img src="/src/assets/service_icons/github.svg" class="size-5" />
       </span>
       <span class="px-2">
-        <Link :href="`https://github.com/${githubId}`" new-tab>@{{ githubId }}</Link>
+        <Link v-if="isGithubIdValid" :href="`https://github.com/${githubId}`" new-tab
+          >@{{ githubId }}</Link
+        >
+        <span v-else>@{{ githubId }}</span>
       </span>
     </div>
     <div v-if="xId" class="flex items-center">
@@ -31,7 +66,10 @@ const selfIntroduction = computed(() => user?.selfIntroduction ?? '')
         <img src="/src/assets/service_icons/x.svg" class="size-5" />
       </span>
       <span class="px-2">
-        <Link :href="`https://x.com/intent/user?screen_name=${xId}`" new-tab>@{{ xId }}</Link>
+        <Link v-if="isXIdValid" :href="`https://x.com/intent/user?screen_name=${xId}`" new-tab
+          >@{{ xId }}</Link
+        >
+        <span v-else>@{{ xId }}</span>
       </span>
     </div>
     <div v-if="traqId" class="flex items-center">
